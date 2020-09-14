2e4cdec6718ac993835b4ce259ac788e4f51a32a
Thiago Lima <thitheguy@gmail.com>
2020-09-14 19:27:15 -0300

    Working with native elements

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, ProgressViewIOS } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ProgressViewIOS, Button } from 'react-native';
 
 export default function App() {
+  const onButtonPress = () => {
+    console.log(`${new Date().toLocaleTimeString()} button press.`)
+  }
+
   return (
     <View style={styles.container}>
       <ProgressViewIOS progress={0.5} />
       <Text style={styles.texts}>Release My Book App!</Text>
       <Text style={styles.texts}>Write and find out!</Text>
       <ActivityIndicator size="large" color="#61DBfB" />
+      <Button title="Click me" onPress={onButtonPress}/>
       <StatusBar style="auto" />
     </View>
   );
