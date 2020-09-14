1a20bf32929044182593019f11d4a6dbb9939879
Thiago Lima <thitheguy@gmail.com>
2020-09-14 19:06:00 -0300

    Working with native elements

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.texts}>Release My Book App!</Text>
       <Text style={styles.texts}>Write and find out!</Text>
+      <ActivityIndicator size="large" color="#61DBfB" />
       <StatusBar style="auto" />
     </View>
   );
@@ -21,5 +22,8 @@ const styles = StyleSheet.create({
   },
   texts: {
     color: '#333DDD'
+  },
+  activity: {
+    color: '#61DBfB'
   }
 });
